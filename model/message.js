be5538e11ce9ed73ef956641b5a1d53ff33d44ae
xiao11lang <1023819715@qq.com>
2019-06-14 18:48:59 +0800

    消息区分未读已读

--- model/message.js
+++ model/message.js
@@ -1,52 +1,80 @@
-const {STRING,INTEGER}=require('sequelize')
-const sequelize=require('./instance')
-const Message=sequelize.define('message',{
-    type:{
-        type:STRING(30)
-    },
-    title:{
-        type:STRING(30)
-    },
-    content:{
-        type:STRING(1000),
-    },
-    toWho:{
-        type:INTEGER
+const { STRING, INTEGER } = require("sequelize");
+const sequelize = require("./instance");
+const Message = sequelize.define("message", {
+  type: {
+    type: STRING(30)
+  },
+  title: {
+    type: STRING(30)
+  },
+  content: {
+    type: STRING(1000)
+  },
+  toWho: {
+    type: INTEGER
+  },
+  hasRead: {
+    type: INTEGER,
+    defaultValue: 0
+  }
+});
+function insert(data) {
+  return Message.sync().then(() => {
+    return Message.create(data);
+  });
+}
+function findByUesrId(id) {
+  return Message.findAll({
+    where: {
+      toWho: id
     }
-})
-function insert(data){
-    return Message.sync().then(()=>{
-        return Message.create(data)
-    })
+  });
 }
-function findByUesrId(id){
-    return Message.findAll({
-        where:{
-            toWho:id
-        }
-    })
-}
-function getDifferentMesCount(id){
-    return sequelize.query(`select type,count(distinct(type)) count from messages group by type,toWho having toWho=${id}`).then((res)=>{
-        return res[0].reduce((pre,cur)=>{
-            pre[cur.type]=cur.count
-            return pre
-        },{})
-    }).then((res)=>{
-        ['official','project','person'].forEach((type)=>{
-            if(!res[type]){
-                res[type]=0
-            }
+function getDifferentMesCount(id) {
+  return sequelize
+    .query(
+      `select type,hasRead,count(distinct(type)) count from messages group by type,toWho,hasRead having toWho=${id}`
+    )
+    .then(res => {
+      return res[0]
+        .filter(row => {
+          return !row.hasRead;
         })
-        return res
+        .reduce((pre, cur) => {
+          pre[cur.type] = cur.count;
+          return pre;
+        }, {});
     })
+    .then(res => {
+      ["official", "project", "person"].forEach(type => {
+        if (!res[type]) {
+          res[type] = 0;
+        }
+      });
+      return res;
+    });
 }
-function getMesListByType(rules){
-    return Message.findAll({
+function getMesListByType(rules) {
+  return Message.findAll({
+    where: {
+      type: rules.type,
+      toWho: rules.id
+    }
+  });
+}
+function setMesRead(id){
+    return Message.update({
+        hasRead:1
+    },{
         where:{
-            type:rules.type,
-            toWho:rules.id
+            id:id
         }
     })
 }
-module.exports={insert,findByUesrId,getDifferentMesCount,getMesListByType}
+module.exports = {
+  insert,
+  findByUesrId,
+  getDifferentMesCount,
+  getMesListByType,
+  setMesRead
+};
--- route/message.js
+++ route/message.js
@@ -1,4 +1,8 @@
-const { findByUesrId,getMesListByType } = require("../model/message");
+const {
+  findByUesrId,
+  getMesListByType,
+  setMesRead
+} = require("../model/message");
 async function getMessage(ctx) {
   const { id } = ctx.request.body;
   let res = await findByUesrId(id);
@@ -10,21 +14,39 @@ async function getMessage(ctx) {
   };
 }
 async function getMessageList(ctx) {
-    const { id,type } = ctx.request.body;
-    let res = await getMesListByType({id,type});
-    res = res.map((value,index) => Object.assign({},value.dataValues,{index:index}));
-    ctx.body = {
-      status: 1,
-      detail: "获取消息成功",
-      list: res
-    };
+  const { id, type } = ctx.request.body;
+  let res = await getMesListByType({ id, type });
+  res = res.map((value, index) =>
+    Object.assign({}, value.dataValues, { index: index })
+  );
+  ctx.body = {
+    status: 1,
+    detail: "获取消息成功",
+    list: res
+  };
+}
+async function changeMesState(ctx) {
+  const { id } = ctx.request.body;
+  await setMesRead(id);
+  ctx.body = {
+    status: 1,
+    detail: "消息已阅读"
+  };
+}
+module.exports = [
+  {
+    method: "post",
+    handler: getMessage,
+    path: "/getMessage"
+  },
+  {
+    method: "post",
+    handler: getMessageList,
+    path: "/getMessageList"
+  },
+  {
+    method: "post",
+    handler: changeMesState,
+    path: "/changeMesState"
   }
-module.exports=[{
-    method:'post',
-    handler:getMessage,
-    path:'/getMessage'
-},{
-    method:'post',
-    handler:getMessageList,
-    path:'/getMessageList'
-}]
+];
